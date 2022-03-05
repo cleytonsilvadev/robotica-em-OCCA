e8e7b6c3e35985a77ae8612eeb506cc441333f8a
cleytonsilvadev <cleyton200696@gmail.com>
2022-03-05 11:32:21 -0300

    Update ligarLeddaPlaca.ino

--- ligarLeddaPlaca/ligarLeddaPlaca.ino
+++ ligarLeddaPlaca/ligarLeddaPlaca.ino
@@ -1,13 +1,27 @@
-
+int contador=0;
 void setup() {
   // localização do led em qual porta ele esta
   pinMode(LED_BUILTIN, OUTPUT);
+  Serial.begin(9600);
+  
 }
 
 // comando de repetição 
 void loop() {
+  
+  
+  
+  
+  
   digitalWrite(LED_BUILTIN, HIGH);   // ligar o led
-  delay(150);                       // tempo para ficar ligado
+  
+  contador = contador+1;
+  if(contador>=6)
+  contador=0;
+  Serial.println(contador);
+  Serial.println("vezes que o led piscou" );
+  delay(800);                       // tempo para ficar ligado
+  
   digitalWrite(LED_BUILTIN, LOW);    // led ira desligar
-  delay(100);                       // tempo ira ficar apagado
+  delay(800);                       // tempo ira ficar apagado
 }
